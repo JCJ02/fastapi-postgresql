974c23daebc53c70c55f588df7dd588790578c49
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-03 09:22:30 +0800

    Feat: Refactor

--- api/main.py
+++ api/main.py
@@ -1,12 +1,39 @@
-from fastapi import FastAPI, HTTPException, Depends
-from api.utilities.database_connection import engine, SessionLocal, get_database
-from api.models.user_model import Base
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.utilities.app_response import AppResponse
+from api.configurations.configuration import configuration
 
+# INITIALIZE FastAPI APP
 app = FastAPI(
     title = "FastAPI with PostgreSQL",
-    description = "API Template"
+    description = "FastAPI Template",
+    version = "1.0.0",
 )
 
-@app.on_event("startup")
-def startup_event():
-    Base.metadata.create_all(bind=engine)
+# MIDDLEWARE FOR CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Update for production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ROOT ENDPOINT
+@app.get("/")
+async def root():   
+    return AppResponse.send_successful(
+        data = None,
+        message = "Welcome to the FastAPI Template",
+        code = 200
+    )
+
+# # RUN THE SERVER
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "api.main:app",
+        port = int(configuration["app"]["port"]),
+        reload = True,
+    )
