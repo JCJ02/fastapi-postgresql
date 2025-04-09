79f7495ef587fcdfd7ed09f2b63083f1521ea7a6
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-10 01:02:26 +0800

    Fix: Imports

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.utilities.app_response import AppResponse
 from api.configurations.configuration import configuration
-from api.routes.v1 import router as api_router
+from api.routes import router as api_router
 
 # INITIALIZE FastAPI APP
 app = FastAPI(
