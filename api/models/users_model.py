cc7f05996600618709b8f41f006c385e761eb02a
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-12 14:45:04 +0800

    Fix: Change file name from user_model to users_model

--- api/models/users_model.py
+++ api/models/users_model.py
@@ -6,7 +6,7 @@ from . import Base
 class Users(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     firstname = Column(String, index=True)
     lastname = Column(String, index=True)
     email_address = Column(String, index=True)
@@ -20,7 +20,7 @@ class Users(Base):
 class Accounts(Base):
     __tablename__ = 'accounts'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     password = Column(String, nullable=True, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
@@ -32,7 +32,7 @@ class Accounts(Base):
 class Roles(Base):
     __tablename__ = 'roles'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     role = Column(String, index=True)
     description = Column(String, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
