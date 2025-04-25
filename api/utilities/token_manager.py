b8d7de299b5d80b62a318ba35aa27010242669c2
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-25 15:58:43 +0800

    feat: create generate and verifty access and refresh token functions

---
+++ api/utilities/token_manager.py
@@ -0,0 +1,12 @@
+class TokenManager:
+    async def generate_access_token():
+        return None
+    
+    async def generate_refresh_token():
+        return None
+    
+    async def verify_access_token():
+        return None
+    
+    async def verify_refresh_token():
+        return None
